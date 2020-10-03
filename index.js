879a348c0ba7f35697e3dde70ad5ecf4b61bf60b
Manel Ouachek <manelouachek@Manel-Ouachek-MacBookPro.local>
2020-10-03 22:33:15 +0200

    class

--- index.js
+++ index.js
@@ -49,6 +49,24 @@ creerPlateau();
 const nombreobstacles = 6, // On veut 6 obstacles sur le plateau
     listearmes = [];
 
+class Player {
+  constructor(initial_pos_x, initial_pos_y) {
+    this.vie = 100;
+    this.degat = 10;
+
+    this.pos_x = initial_pos_x;
+    this.pos_y = initial_pos_y;
+  }
+
+  deplacer(direction) {
+    if (direction === 'left') {
+      this.pos_x - 1;
+    }
+  }
+}
+
+const player1 = new Player(2, 5);
+
 const player1 = 1, //
     player1tab = [];
 
